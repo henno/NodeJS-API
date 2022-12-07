8f09a1fc25dacde6fe9198ed1dc3d52efc4157a1
henno <henno.taht@gmail.com>
2022-12-07 13:14:07 +0200

    Improve error handling

--- index.js
+++ index.js
@@ -14,23 +14,28 @@ app.use(cors());
 
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 app.get("/", (req, res) => {
-  res.json({ message: "ok" });
+    res.json({message: "ok"});
 });
 
 app.use("/comments", commentsRouter);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
-  if (!err.code) console.error(err.message, err.stack);
-  res.status(err.code || 500).send({message: err.message});
+
+    // Console log the error if error code is missing or not a number
+    if (!err.code || isNaN(err.code)) console.error(err.message, err.stack);
+
+    // Use error code 500 if error code is missing or not a number and return error message
+    res.status(parseInt(err.code) || 500).send({message: err.code + ': '+ err.message});
+
 });
 
 app.listen(port, () => {
-  console.log(`Server started. Docs at http://localhost:${port}/docs`);
+    console.log(`Server started. Docs at http://localhost:${port}/docs`);
 });
